a46c7d8b6abf1733a8cbfc1cb6387491d38b11a2
gregroyclark <gregroyclark@gmail.com>
2021-03-28 11:13:04 -0500

    added ProductList component

---
+++ src/Components/ProductList.js
@@ -0,0 +1,30 @@
+const ProductList = () => {
+    return (
+        <div>
+            <div>
+                <p>Lemons</p>
+                <div>
+                    <button>+</button>
+                    <h4>0</h4>
+                    <button>-</button>
+                </div>
+            </div>
+            <div>
+                <p>Sugar</p>
+                <div>
+                    <button>+</button>
+                    <h4>0</h4>
+                    <button>-</button>
+                </div>
+            </div>
+            <div>
+                <p>Ice Cubes</p>
+                <div>
+                    <button>+</button>
+                    <h4>0</h4>
+                    <button>-</button>
+                </div>
+            </div>
+        </div>
+    )
+}
