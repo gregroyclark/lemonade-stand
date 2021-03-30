e5175dddf42eba8ec4e4bf9522d9fafe2006d3b7
gregroyclark <gregroyclark@gmail.com>
2021-03-30 00:29:03 -0500

    added state to Lemons

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import './ProductList.css'
 
 const ProductList = () => {
+    const [numberOfLemons, updateNumberOfLemons] = useState(0)
+
     return (
         <div className="ProductList">
             <div className="Product">
                 <p>Lemons</p>
                 <div className="Count">
-                    <button>+</button>
-                    <h4>0</h4>
-                    <button>-</button>
+                    <button onClick={() => updateNumberOfLemons(numberOfLemons - 1)}>-</button>
+                    <h4>{numberOfLemons}</h4>
+                    <button onClick={() => updateNumberOfLemons(numberOfLemons + 1)}>+</button>
                 </div>
             </div>
             <div className="Product">
