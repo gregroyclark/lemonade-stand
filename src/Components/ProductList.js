f17f5d17581a37a9f1d0c17f58f24d0718d23030
gregroyclark <gregroyclark@gmail.com>
2021-03-30 00:06:38 -0500

    imported CSS, added classNames to divs

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,25 +1,27 @@
+import './ProductList.css'
+
 const ProductList = () => {
     return (
-        <div>
-            <div>
+        <div className="ProductList">
+            <div className="Product">
                 <p>Lemons</p>
-                <div>
+                <div className="Count">
                     <button>+</button>
                     <h4>0</h4>
                     <button>-</button>
                 </div>
             </div>
-            <div>
+            <div className="Product">
                 <p>Sugar</p>
-                <div>
+                <div className="Count">
                     <button>+</button>
                     <h4>0</h4>
                     <button>-</button>
                 </div>
             </div>
-            <div>
+            <div className="Product">
                 <p>Ice Cubes</p>
-                <div>
+                <div className="Count">
                     <button>+</button>
                     <h4>0</h4>
                     <button>-</button>
@@ -28,3 +30,5 @@ const ProductList = () => {
         </div>
     )
 }
+
+export default ProductList
